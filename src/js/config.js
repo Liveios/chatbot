7d34bcc90fb2d260b8a5ab2bd70a74401d2aadce
ANIODEV <105338184+aniodev@users.noreply.github.com>
2022-06-28 21:02:40 +0530

    added functions
    added functions for open, close and redirect

--- src/js/config.js
+++ src/js/config.js
@@ -1,12 +1,20 @@
-const openNav = function () {
-    let nav = document.querySelector(".nav");
+const open_nav = function () {
+    const nav = document.querySelector(".nav");
     nav.style.width = "100%";
 }
 
-const closeNav = function () {
-    let nav = document.querySelector(".nav");
+
+const close_nav = function () {
+    const nav = document.querySelector(".nav");
     nav.style.width = "0%";
 }
+
+const go = function (url) {
+    window.location = "https://" + url;
+}
+
+
+
 
 const showCopyright = function () {
     let footer = document.querySelector(".footer");
